7c05aaf6852cbc9e74cba328583a7cce4dec3f6c
Chip <chip.elmer@gmail.com>
2019-07-08 14:09:26 -0500

    Add project files.

---
+++ CalculatorWithTests/Calculator.cs
@@ -0,0 +1,8 @@
+﻿
+namespace CalculatorWithTests
+{
+    class Calculator
+    {
+        
+    }
+}
---
+++ CalculatorWithTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorWithTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
